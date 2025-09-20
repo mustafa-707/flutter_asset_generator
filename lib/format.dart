c11cfcbbc1f62dd9fbf8802b0371d7fa803f994a
Mustafa <mustafa@device-103.home>
2025-09-20 11:36:37 +0300

    upgrade versions

--- lib/format.dart
+++ lib/format.dart
@@ -1,6 +1,9 @@
 import 'package:dart_style/dart_style.dart';
+import 'package:pub_semver/pub_semver.dart';
 
 String formatFile(String source) {
-  final DartFormatter df = DartFormatter();
+  final DartFormatter df = DartFormatter(
+    languageVersion: Version.parse('3.3.0'),
+  );
   return df.format(source);
 }
