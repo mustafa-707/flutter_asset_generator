6b7a6bb4d3917997ce3dfed441078271a75aeb32
Mustafa <mustafa@device-103.home>
2025-09-20 11:48:22 +0300

    dart version required

--- lib/format.dart
+++ lib/format.dart
@@ -1,9 +1,8 @@
 import 'package:dart_style/dart_style.dart';
-import 'package:pub_semver/pub_semver.dart';
 
 String formatFile(String source) {
   final DartFormatter df = DartFormatter(
-    languageVersion: Version.parse('3.3.0'),
+    languageVersion: DartFormatter.latestLanguageVersion,
   );
   return df.format(source);
 }
